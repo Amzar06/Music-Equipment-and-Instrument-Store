02911ae1efaae70aebfa2c1ce1b969d0e050e170
devvyaa <devvyaa.ponnudurai@student.mmu.edu.my>
2026-05-30 00:24:18 +0800

    Update home_page.php

--- customer/home_page.php
+++ customer/home_page.php
@@ -11,11 +11,14 @@
       <h2>Welcome User</h2>
       <p>Select an option below</p>
 
+      <div class="text-center mt-4" style="font-size: 14px;">
     <a href="edit_profile_page.php"><button>Edit Profile</button></a><br><br>
+    <div class="text-center mt-4" style="font-size: 14px;">
       <a href="change_password.php"><button>Change Password</button></a><br><br>
-      <a href="product_details.php"><button>View Products</button></a><br><br>
       <div class="text-center mt-4" style="font-size: 14px;">
       <a href="product_details.php"><button>View Products</button></a><br><br>
+      <div class="text-center mt-4" style="font-size: 14px;">
+      <a href="product_details.php"><button>Log Out</button></a><br><br>
     </div>
   </div>
 </body>
