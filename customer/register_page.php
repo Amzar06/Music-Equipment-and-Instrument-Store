6dbbac423907ee6a8d84c0585cac6f57b275679f
devvyaa <devvyaa.ponnudurai@student.mmu.edu.my>
2026-05-29 23:43:01 +0800

    Update register_page.php

--- customer/register_page.php
+++ customer/register_page.php
@@ -12,15 +12,19 @@
       <p>Register a new account</p>
 
       <label>Full Name</label>
+
       <input type="text" placeholder="Enter your full name">
 
       <label>Email Address</label>
+
       <input type="email" placeholder="Enter your email">
 
       <label>Password</label>
+
       <input type="password" placeholder="Enter your password">
 
       <label>Confirm Password</label>
+
       <input type="password" placeholder="Confirm your password">
 
       <a href="/Music-Equipment-and-Instrument-Store/product/cust login.php"><button>Register</button></a>
