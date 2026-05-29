c8ff6e8711247732ed96d7d3500b57f7cfaec726
devvyaa <devvyaa.ponnudurai@student.mmu.edu.my>
2026-05-29 18:44:25 +0800

    Update product_details_page.php

--- customer/product_details_page.php
+++ customer/product_details_page.php
@@ -2,7 +2,7 @@
 <html>
 <head>
   <title>Product Details</title>
-  <link rel="stylesheet" href="style.css">
+  <link rel="stylesheet" href="customer.css">
 </head>
 <body>
   <div class="header">ADMIN PORTAL</div>
