2cba87e9d7cf8b6142888e4a23fb6f9db00e64ce
devvyaa <devvyaa.ponnudurai@student.mmu.edu.my>
2026-05-29 23:44:53 +0800

    Update register_page.php

--- customer/register_page.php
+++ customer/register_page.php
@@ -27,7 +27,7 @@
 
       <input type="password" placeholder="Confirm your password">
 
-      <a href="/Music-Equipment-and-Instrument-Store/product/cust login.php"><button>Register</button></a>
+      <a href="/Music-Equipment-and-Instrument-Store/product/cust login.php">Register</a>
     </div>
   </div>
 
