eb43fbe4b88d75b9cf6f5d9f073cfd658b57eaee
devvyaa <devvyaa.ponnudurai@student.mmu.edu.my>
2026-05-29 23:56:55 +0800

    Update home_page.php

--- customer/home_page.php
+++ customer/home_page.php
@@ -12,7 +12,7 @@
       <p>Select an option below</p>
     <a href="edit-profile_page.php"><button>Edit Profile</button></a><br><br>
       <a href="change-password.php"><button>Change Password</button></a><br><br>
-      <a href="product-details.php"><button>View Products</button></a><br><br>
+      <a href="product-details.php"><button>      View Products     </button></a><br><br>
       <button href="logout_page.php">Register</button>
     </div>
   </div>
