70aabb86e91a3a0ba7c182af513c610161c968db
devvyaa <devvyaa.ponnudurai@student.mmu.edu.my>
2026-05-30 00:03:19 +0800

    Update home_page.php

--- customer/home_page.php
+++ customer/home_page.php
@@ -10,10 +10,10 @@
     <div class="card">
       <h2>Welcome User</h2>
       <p>Select an option below</p>
-    <a href="edit-profile_page.php"><button>Edit Profile</button></a><br><br>
-      <a href="change-password.php"><button>Change Password</button></a><br><br>
-      <a href="product-details.php"><button>      View Products     </button></a><br><br>
-      <button href="logout_page.php">Register</button>
+    <a href="edit_profile_page.php"><button>Edit Profile</button></a><br><br>
+      <a href="change_password.php"><button>Change Password</button></a><br><br>
+      <a href="product_details.php"><button>View Products</button></a><br><br>
+       <a href="logout.html"><button>Logout</button></a>
     </div>
   </div>
 </body>
