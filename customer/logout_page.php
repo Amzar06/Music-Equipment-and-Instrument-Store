4937b599a86beeeb13671eb448bafb1e4db33392
devvyaa <devvyaa.ponnudurai@student.mmu.edu.my>
2026-05-29 18:44:21 +0800

    Update logout_page.php

--- customer/logout_page.php
+++ customer/logout_page.php
@@ -2,7 +2,7 @@
 <html>
 <head>
   <title>Logout</title>
-  <link rel="stylesheet" href="style.css">
+  <link rel="stylesheet" href="customer.css">
 </head>
 <body>
   <div class="header">ADMIN PORTAL</div>
