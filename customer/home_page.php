b715fbab97b7a6c783f76d28f01b54bc982c43de
devvyaa <devvyaa.ponnudurai@student.mmu.edu.my>
2026-06-01 15:47:45 +0800

    Update home_page.php

--- customer/home_page.php
+++ customer/home_page.php
@@ -9,9 +9,10 @@
   <div class="container">
     <div class="card">
       <h2>Welcome User</h2>
-      <p>Select an option below</p>
+      <p>Select an option below:  /p>
 
-      <div class="text-center mt-6" style="font-size: 14px;">
+     <div style="display: flex; flex-direction: column; align-items: center; gap: 15px;">
+    <button style="width: 200px; padding: 10px 0; background-color: #1d61f2; color: white; border: none; border-radius: 8px; font-weight: bold; cursor: pointer;">
     <a href="edit_profile_page.php"><button>Edit Profile</button></a><br><br>
     <div class="text-center mt-4" style="font-size: 14px;">
       <a href="change_password.php"><button>Change Password</button></a><br><br>
