6a7af64a6236019c0d06bf19dabdd29cf1af1d07
devvyaa <devvyaa.ponnudurai@student.mmu.edu.my>
2026-06-02 18:25:34 +0800

    Create index.php

---
+++ customer/index.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Customer Portal</title>
+    <link rel="stylesheet" href="customer.css">
+</head>
+<body>
+
+<div class="container">
+    <div class="card">
+
+        <h2>Customer Portal</h2>
+        <p>Welcome to the system</p>
+
+        <a href="cust_login.php">
+            <button type="button">Login</button>
+        </a>
+
+        <br><br>
+
+        <a href="customer_register_page.php">
+            <button type="button">Register</button>
+        </a>
+
+    </div>
+</div>
+
+</body>
+</html>
