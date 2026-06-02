2168aab166abede3429e22aee490061f52698db1
devvyaa <devvyaa.ponnudurai@student.mmu.edu.my>
2026-06-02 11:42:52 +0800

    Update logout_page.php

--- customer/logout_page.php
+++ customer/logout_page.php
@@ -1,19 +1,37 @@
+<?php
+session_start();
+
+$_SESSION = array();
+
+
+if (ini_get("session.use_cookies")) {
+    $params = session_get_cookie_params();
+    setcookie(session_name(), '', time() - 42000,
+        $params["path"], $params["domain"],
+        $params["secure"], $params["httponly"]
+    );
+}
+session_destroy();
+?>
 <!DOCTYPE html>
-<html>
+<html lang="en">
 <head>
+  <meta charset="UTF-8">
   <title>Logout</title>
   <link rel="stylesheet" href="customer.css">
 </head>
 <body>
-  <div>
 
   <div class="container">
-    <div class="card">
+    <div class="card" style="text-align: center;">
       <h2>You Have Logged Out</h2>
-      <p>Thank you for using the system</p>
+      <p>Thank you for using the system!</p>
 
-      <a href="/Music-Equipment-and-Instrument-Store/product/cust login.php"><button>Login Again</button></a>
+      <a href="/Music-Equipment-and-Instrument-Store/product/cust login.php">
+        <button type="button">Login Again</button>
+      </a>
     </div>
   </div>
+
 </body>
 </html>
