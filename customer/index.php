7d386a6f0cf3b7fba1f7c555eaac6722a03f0426
devvyaa <devvyaa.ponnudurai@student.mmu.edu.my>
2026-06-02 23:46:41 +0800

    Update index.php

--- customer/index.php
+++ customer/index.php
@@ -1,55 +1,42 @@
-<!-- FEATURED PRODUCTS -->
-<section class="container py-5">
+<div class="row">
 
-    <h2 class="mb-4">Featured Instruments</h2>
-
-    <div class="row">
-
-        <!-- PRODUCT 1 -->
-        <div class="col-md-4 mb-4">
-            <div class="card h-100">
-
-                <div class="card-body text-center">
-                    <h5>Acoustic Guitar</h5>
-                    <p>RM 499.00</p>
-                    <a href="product.php" class="btn btn-dark btn-sm">
-                        View Details
-                    </a>
-                </div>
-
+    <!-- PRODUCT 1 -->
+    <div class="col-md-4 mb-4">
+        <div class="card h-100">
+            <div class="card-body text-center">
+                <h5>Acoustic Guitar</h5>
+                <p>RM 499.00</p>
+                <a href="product.php" class="btn btn-dark btn-sm">
+                    View Details
+                </a>
             </div>
         </div>
-
-        <!-- PRODUCT 2 -->
-        <div class="col-md-4 mb-4">
-            <div class="card h-100">
-
-                <div class="card-body text-center">
-                    <h5>Digital Piano</h5>
-                    <p>RM 1,299.00</p>
-                    <a href="product.php" class="btn btn-dark btn-sm">
-                        View Details
-                    </a>
-                </div>
-
-            </div>
-        </div>
-
-        <!-- PRODUCT 3 -->
-        <div class="col-md-4 mb-4">
-            <div class="card h-100">
-
-                <div class="card-body text-center">
-                    <h5>Drum Set</h5>
-                    <p>RM 899.00</p>
-                    <a href="product.php" class="btn btn-dark btn-sm">
-                        View Details
-                    </a>
-                </div>
-
-            </div>
-        </div>
-
     </div>
 
-</section>
+    <!-- PRODUCT 2 -->
+    <div class="col-md-4 mb-4">
+        <div class="card h-100">
+            <div class="card-body text-center">
+                <h5>Digital Piano</h5>
+                <p>RM 1,299.00</p>
+                <a href="product.php" class="btn btn-dark btn-sm">
+                    View Details
+                </a>
+            </div>
+        </div>
+    </div>
+
+    <!-- PRODUCT 3 -->
+    <div class="col-md-4 mb-4">
+        <div class="card h-100">
+            <div class="card-body text-center">
+                <h5>Drum Set</h5>
+                <p>RM 899.00</p>
+                <a href="product.php" class="btn btn-dark btn-sm">
+                    View Details
+                </a>
+            </div>
+        </div>
+    </div>
+
+</div>
